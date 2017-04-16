f159e69ad2f0ae636876c0207b476fd506112840
Ryder Dain <ryder.dain@gmail.com>
2017-04-16 17:12:12 +0200

    Reducing overhead and combining functions.

---
+++ README.md
@@ -0,0 +1,52 @@
+# aggregator
+
+Python module to create multikey transaction accounting maps.
+
+## History
+
+With lots of transactions to manipulate for financial reporting, I had a
+general problem to solve: I needed a rapid way of assembling transactions on
+the basis of a set of arbitrary values. As the needed totals were clear (total
+numbers of transcations and the sum of those transactions), what was missing
+was a way of quickly aggregating these values in a way that would let me put
+in, and pull out only the data I needed.
+
+Since what I needed in most cases was a dict() built up with each set of
+attributes, the solution finally colasced after some trial and error. The
+result was a single multi-key, dict-like class with a fixed value type.
+
+To maintain fixed sets of keys, we needed a methodology for working with
+tuples. The original design worked with arbitrary keys, and various
+implmentations employed namedtuples heavily in order to clarify the category of
+key in use. After repeated trials with this enforced implementation, it became
+clear that it would be far better to set the fields to be used from the
+beginning, and enforce those fields for the object.
+
+## Design
+
+In its simplest form, an Aggregator is a wrapper around two Counter objects,
+from the standard library collections module. Each counter is a dict in its own
+right, with some extra methods bundled in. The keys in use are employed at each
+level of the object, which is complex to manage at the base level but makes
+adding and retreiving new data very rapid.
+
+As a logical object, it looks a little like this below, with the following
+definitions:
+
+  TupleKey: an arbitrary list of keys in a strict ordering according to the "fields" set during instantiation.
+  CountKey: a collections.namedtuple instance of ("Key", ["count", "amount]) where the count and amount values are each a collections.Counter object.
+  Counter: in this case, a collections.Counter() object with a one-to-one match between its own keys, and the keys of the Aggregator object.
+
+    Aggregator
+      { TupleKey:
+          CountKey("count": Counter(TupleKey: total count),
+                   "amount": Counter(TupleKey: total amount)
+          ),
+        TupleKey:
+          CountKey("count": Counter(TupleKey: total count),
+                   "amount": Counter(TupleKey: total amount)
+          ),
+        ...
+      }
+
+
--- aggregator.py
+++ aggregator.py
@@ -6,33 +6,33 @@ import collections
 import types
 from datetime import date
 
-###############################################################################
+#################################################
 ## Classes
 
-Count = collections.namedtuple('Count', ['count','amount'])
+Count = collections.namedtuple('Total', ['count','amount'])
 DictKeyTypes = (dict,collections.namedtuple,collections.OrderedDict)
 
 class Aggregator(dict):
-    '''Takes an initial parameter to pass to a <collections.nametuple> constructor.
-    This initial paramter must be a list or tuple of "fields" to use for keys in the
-    Aggregator object. Aggregator may then be initialized with any number of
-    arguments matching the fields. Each argument then becomes an empty key.'''
+    '''Takes an initial list of fields to use for tracking keys in the
+       Aggregator object. Aggregator may then be initialized with any number of
+       arguments matching the fields. Each argument then becomes an empty key.'''
+
     def __init__(self, fieldnames, *args, **kwargs):
         self.count = collections.Counter()
         self.amount = collections.Counter()
         self._fields = tuple(fieldnames)
         self._keywrapper = collections.namedtuple('Key', fieldnames, **kwargs)
         for key in args:
-            self[self._keywrapper(*key)] = Count(0, 0.0)
-
+            self[tuple(key)] = Count(0, 0.0)
+            
     def __repr__(self):
-        return 'Aggregate:\n%s' % '\n'.join('  %s: %s' % (k,v) for k,v in self.iteritems())
-
+        return 'Aggregate:\n%s' % '\n'.join('  %s: %s' % (self._keywrapper(*k),v) for k,v in self.iteritems())
+        
     def __len__(self):
         return len(self.count)
-
+        
     def __setitem__(self, key, amount):
-        key = self._keywrapper(*key)
+        key = tuple(key)
         count, amount = getCountObjectFromPair(amount)
         if not self.count.get(key):
             self.count[key], self.amount[key] = count, amount
@@ -40,20 +40,23 @@ class Aggregator(dict):
             self.count = collections.Counter({key: count})
             self.amount = collections.Counter({key: amount})
         super(Aggregator, self).__setitem__(key, Count(self.count[key], self.amount[key]))
-
+        
     def __getitem__(self, key):
-        key = self._keywrapper(*key)
+        key = tuple(key)
         return Count(self.count[key], float(self.amount[key]))
-
+        
     def __radd__(self, aggregate):
-        pass
-
+        self.splice(aggregate)
+        
+    def __add__(self, aggregate):
+        self.splice(aggregate)
+        return self
+        
     def add(self, key, value):
-        key = self._keywrapper(*key)
         self.count.update({key: 1})
         self.amount.update({key: float(value)})
         super(Aggregator, self).__setitem__(key, Count(self.count[key], self.amount[key]))
-
+        
     def update(self, *args, **kwargs):
         '''Expects either a dict of keys and amounts as values, or set as keywords.'''
         if args:
@@ -63,17 +66,17 @@ class Aggregator(dict):
                 self.add(key, value)
         for key, value in kwargs.iteritems():
             self.add(key, value)
-
+            
     def copy(self):
         shallow_copy = Aggregator(self._fields)
-        for key in self:
+        for key in self.iterkeys():
             shallow_copy[key] = self[key]
         return shallow_copy
-
+        
     def fieldkeys(self, field):
         index = self._fields.index(field)
         return set( fk[index] for fk in self.keys() )
-
+        
     def filter(self, *args):
         filtered_copy = Aggregator(self._fields)
         for k in args:
@@ -81,40 +84,60 @@ class Aggregator(dict):
             for key in found:
                 filtered_copy[key] = self[key]
         return filtered_copy
+        
+    def collapse(self, collapse_field): 
+        '''Remove a sub-key element and merge values lacking the key, returning a new Aggregator.'''
+        collapse_index = self._fields.index(collapse_field)
+        fields = list(self._fields)
+        fields.remove(collapse_field)
+        collapsed_copy = Aggregator(fields)
+        for key, value in self.iteritems():
+            key = list(key)
+            toss = key.pop(collapse_index)
+            key = tuple(key)
+            if collapsed_copy.get(key):
+                collapsed_copy.count.update({key: value.count})
+                collapsed_copy.amount.update({key: value.amount})
+                super(Aggregator, collapsed_copy).__setitem__(key, Count(collapsed_copy.count[key], collapsed_copy.amount[key]))
+            else:
+                collapsed_copy[key] = value
+        return collapsed_copy
 
-#   def collapse(self, collapse_field):
-#       '''Remove a sub-key element and merge values lacking the key, returning a new Aggregator.'''
-#       collapse_index = self._fields.index(collapse_field)
-#       fields = list(self._fields)
-#       fields.remove(collapse_field)
-#       collapsed_copy = Aggregator(fields)
-#       for key, value in self.iteritems():
-#           newkey = list(key)
-#           collapsedon = newkey.pop(collapse_index)
-#           collapsed_copy.count.update({ tuple(newkey): value.count})
-#           collapsed_copy.amount.update({ tuple(newkey): value.amount})
-#           super(Aggregator, collapsed_copy).__setitem__(tuple(newkey), Count(collapsed_copy.count[key], collapsed_copy.amount[key]))
-#       return collapsed_copy
-
-    def splice(self, *aggregates):
+    def splice(self, aggregate):
         '''Squeeze in additional aggregators, and add any keys to key tuple given if not present.'''
-        for agg in aggregates:  # first, consistency checks
-            if type(agg) is not type(self):
-                raise ValueError("Can only splice other %s objects." % type(self))
-            if agg._fields != self._fields:
-                raise ValueError("Can only splice with fieldlist: %s." % repr(list(self._fields)))
-            for key, aggCount in agg.iteritems():
-                self.count.update({key: aggCount.count})
-                self.amount.update({key: aggCount.amount})
-                super(Aggregator, self).__setitem__(key, Count(self.count[key], self.amount[key]))
-
+        # first, consistency checks
+        if type(agg) is not type(self):
+            raise ValueError("Can only splice other %s objects." % type(self))
+        if agg._fields != self._fields:
+            raise ValueError("Can only splice with fieldlist: %s." % repr(list(self._fields)))
+        for key, aggCount in agg.iteritems():
+            self.count.update({key: aggCount.count})
+            self.amount.update({key: aggCount.amount})
+            super(Aggregator, self).__setitem__(key, Count(self.count[key], self.amount[key]))
+                
     def value_sorted(self, by_count=False, reverse=False):
         return sorted(self.iteritems(), key=lambda (k,v): (v.count, v.amount) if by_count else (v.amount, v.count), reverse=reverse)
+        
+    def field_sorted(self, *field_keys, **kwargs):
+        r = kwargs.get('reverse') or False
+        return sorted(self.iteritems(), key=lambda (k,v): [k._asdict[fk] for fk in field_keys], reverse=r)
 
+    def getcsv(self, *sort_keys, **kwargs):
+        csv_fd = StringIO()
+        r = kwargs.pop('reverse') if kwargs.get('reverse') else False
+        # leaving this open to **kwargs for passing in alternate dialects
+        cw = csv.writer(csv_fd, **kwargs)
+        cw.writerow(self._fields + ('count', 'amount'))
+        for pair in self.field_sorted(*sort_keys, reverse=r):
+            # each row is a (Key, Count) pair.
+            cw.writerow([i._asdict.values() for i in pair])
+        csv_fd.seek(0)
+        return csv_fd
 
-###############################################################################
-##                               Functions
-###############################################################################
+        
+#################################################
+##                Functions
+#################################################
 
 def getCountObjectFromPair(tup):
     try:
@@ -125,7 +148,7 @@ def getCountObjectFromPair(tup):
             raise TypeError("Expected length 2, got %d" % len(tup))
         count, amount = int(tup[0]), float(tup[1])
         return Count(count, amount)
-
+        
 def getComplexCountFromSqlQuery(query, cursor, keylist):
     SqlMap = collections.Counter()
     cursor.execute(query)
@@ -140,7 +163,7 @@ def getComplexCountFromSqlQuery(query, cursor, keylist):
             count = int(value) if type(value) in (int,long) or (type(value) in (str,unicode) and value.isdigit()) else float(value)
         SqlMap[tuple(key)] = count
     return SqlMap
-
+    
 def getSetDictFromSqlQuery(query, cursor, keylist):
     SqlMap = dict()
     cursor.execute(query)
@@ -158,7 +181,7 @@ def getSetDictFromSqlQuery(query, cursor, keylist):
         else:
             SqlMap[tuple(key)].update(item)
     return SqlMap
-
+    
 def getAggregatesFromSqlQuery(query, cursor, keylist):
     SqlMap = Aggregator(keylist)
     cursor.execute(query)
@@ -177,3 +200,26 @@ def getAggregatesFromSqlQuery(query, cursor, keylist):
                 count = int(value)
         SqlMap[tuple(key)] = Count(count, amount)
     return SqlMap
+    
+    
+if __name__ == '__main__':
+    import random
+    agg = Aggregator(['field1', 'field2', 'field3'])
+    
+    ccy = ('EUR', 'GBP', 'PLN', 'USD')
+    land = ('de', 'es', 'fr', 'it', 'nl', 'pt')
+    method = ('pos','atm')
+
+    for n in xrange(20):
+        f1 = ccy[random.randrange(0,len(ccy))]
+        f2 = land[random.randrange(0,len(land))]
+        f3 = method[random.randrange(0,len(method))]
+        agg.update({(f1,f2,f3): n*float('%01.2f' % random.random())})
+
+    print(agg)
+    print(agg.collapse('field2'))
+    print(agg.filter('USD'))
+
+    agg2 = agg.filter('USD')
+    print(agg + agg2)
+
